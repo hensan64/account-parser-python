b6cc59c78c58d01553c1958c5d807b2c3d1cfc52
Henrik Sandström I <henrik.i.sandstrom@ericsson.com>
2016-05-19 21:35:22 +0200

    Added .gitignore for .idea directory

---
+++ accountparser/account_data.py
@@ -0,0 +1,26 @@
+# Account data
+
+# -----------
+# Data format
+# -----------
+# year:   '2011'
+# month:  '06'
+# day:    '12'
+# prefix: 'SB'
+# memo:   'Descriptive Text'
+# type:   :debit | :credit
+# value:  '1234.56'
+
+class AccountData():
+
+    def __init__(self, year, month, day, prefix, memo, type, value):
+        self.year = year
+        self.month = month
+        self.day = day
+        self.prefix = prefix
+        self.memo = memo
+        self.type = type
+        self.value = value
+
+    def dummy(self):
+        print('Dummy')
