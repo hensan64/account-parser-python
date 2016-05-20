0e9b4cce2577e3bc9c375621b6ce10954698fce5
Henrik Sandström I <henrik.i.sandstrom@ericsson.com>
2016-05-20 17:39:47 +0200

    Added .gitignore for .idea directory

---
+++ accountparser/lib.py
@@ -0,0 +1,18 @@
+
+import re
+
+def isBlank(line):
+    pattern = re.compile(r'^\s*$')
+    if pattern.match(line):
+        return True
+    else:
+        return False
+
+def format_memo(string):
+    string = re.sub(r',', r' ', string)
+    return re.sub(r'\s{2,}', ' ', string)
+
+
+def format_value(string):
+    string = re.sub(r'[.\s]', r'', string)
+    return re.sub(r',', r'.', string)
